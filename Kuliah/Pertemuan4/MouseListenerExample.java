35387d0c0a50ac4ae0fdb98319df2e765586952d
Renaajl <muhammadraflyalfrz24@gmail.com>
2024-10-14 08:22:19 +0700

    Event Listener

---
+++ Kuliah/Pertemuan4/MouseListenerExample.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class MouseListenerExample {
+    public static void main (String[] args) {
+        // Membuat frame
+        JFrame frame = new JFrame("MouseListener Example");
+
+        // Membuat label untuk menampilkan pesan
+        JLabel label = new JLabel("Arahkan dan klik mouse pada area ini. ");
+        label.setBounds(50,50,300,30);
+
+        // Menambahkan MouseListener ke label
+        label.addMouseListener(new MouseListener() {
+            // Dijalankan Ketika mouse diklik (klik kiri, kanan atau tengah)
+            public void mouseClicked(MouseEvent e) {
+                label.setText("Mouse Clicked at: (" + e.getX() + ", " + e.getY() + ")");
+            }
+
+            // Dijalankan ketika mouse ditekan (tanpa dilepaskan)
+            public void mousePressed(MouseEvent e) {
+                label.setText("Mouse Pressed at: (" + e.getX() + ", " + e.getY() + ")");
+            }
+
+            // Dijalankan ketika mouse dileaskan setelah ditekan
+            public void mouseReleased(MouseEvent e) {
+                label.setText("Mouse Released at: (" + e.getX() + ", " + e.getY() + ")");
+            }
+
+            // Dijalankan ketika mouse masuk area komponen
+            public void mouseEntered(MouseEvent e) {
+                label.setText("Mouse Entered the area. ");
+            }
+
+            // Dijalankan ketika mouse keluar dari area komponen
+            public void mouseExited(MouseEvent e) {
+                label.setText("Mouse Exited the area. ");
+            }
+        });
+
+        // menambahkan label ke frame
+        frame.add(label);
+
+        //setting frame
+        frame.setSize(400,200);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
